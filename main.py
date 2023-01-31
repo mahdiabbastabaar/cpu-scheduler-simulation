6593ea35811fd5d9f4adf2325ff9a7c6e0c2f133
Mehdi Abbastabaar <m.abbastabaar@digikala.com>
2023-02-01 02:27:45 +0330

    the initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import scheduler    
+import simpy
+
+
+def main():
+    print("Hello World")
+    X, Y = [float(x) for x in input().split()]
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
